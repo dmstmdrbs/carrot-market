73412091450d01b61bce711f39b87dfca9c9c470
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-05-11 01:05:54 +0900

    토큰 생성 로직 추가

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -20,53 +20,37 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 	//
 
 	const payload = phone ? { phone: +phone } : { email };
-	const user = await client.user.upsert({
-		where: {
-			...payload,
+	// const user = await client.user.upsert({
+	// 	where: {
+	// 		...payload,
+	// 	},
+	// 	create: {
+	// 		...payload,
+	// 		name: "Anonymous",
+	// 	},
+	// 	update: {},
+	// });
+	// console.log(user);
+	// token을 받아와야함
+
+	const token = await client.token.create({
+		data: {
+			payload: "1234",
+			user: {
+				connectOrCreate: {
+					// 조건에 만족하는 유저가 있으면 토큰과 연결하고, 없으면 유저를 만들고 토큰을 만든다
+					where: {
+						...payload,
+					},
+					create: {
+						name: "Anonymous",
+						...payload,
+					},
+				},
+			},
 		},
-		create: {
-			...payload,
-			name: "Anonymous",
-		},
-		update: {},
 	});
-	console.log(user);
-	// if (email) {
-	// 	user = await client.user.findUnique({
-	// 		where: {
-	// 			email,
-	// 		},
-	// 	});
-	// 	if (user) console.log("user found");
-	// 	if (!user) {
-	// 		console.log("not found user, create user");
-	// 		user = await client.user.create({
-	// 			data: {
-	// 				name: "Anonymous",
-	// 				email,
-	// 			},
-	// 		});
-	// 	}
-	// 	console.log(user);
-	// }
-	// if (phone) {
-	// 	user = await client.user.findUnique({
-	// 		where: {
-	// 			phone: +phone,
-	// 		},
-	// 	});
-	// 	if (user) console.log("user found");
-	// 	if (!user) {
-	// 		console.log("not found user, create user");
-	// 		user = await client.user.create({
-	// 			data: {
-	// 				name: "Anonymous",
-	// 				phone: +phone,
-	// 			},
-	// 		});
-	// 	}
-	// 	console.log(user);
-	// }
+	console.log(token);
 	return res.status(200).end();
 }
 
