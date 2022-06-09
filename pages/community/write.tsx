208ec224c55a7af6068790e3e69de2b5401a561d
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-06-09 17:03:22 +0900

    동네생활 게시글 연결

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -2,15 +2,46 @@ import type { NextPage } from "next";
 import Button from "@components/button/button";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutations";
+import { useEffect } from "react";
+import { Post } from "@prisma/client";
+import { useRouter } from "next/router";
+
+interface WriteForm {
+  question: string;
+}
+interface WriteResponse {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPage = () => {
-	return (
-		<Layout canGoBack title="Write Post">
-			<form className="p-4 space-y-4">
-				<TextArea required placeholder="Ask a question!" />
-				<Button text="Submit" />
-			</form>
-		</Layout>
-	);
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<WriteForm>();
+  const [makePost, { loading, data }] = useMutation<WriteResponse>("/api/posts");
+  const onValid = (data: WriteForm) => {
+    if (loading) return; // 여러번 클릭하는 것을 막기 위함
+    makePost(data);
+  };
+
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/community/${data.post.id}`);
+    }
+  }, [data, router]);
+
+  return (
+    <Layout canGoBack title="Write Post">
+      <form className="p-4 space-y-4" onSubmit={handleSubmit(onValid)}>
+        <TextArea
+          register={register("question", { required: true, minLength: 5 })}
+          required
+          placeholder="Ask a question!"
+        />
+        <Button text={loading ? "Posting..." : "Submit"} />
+      </form>
+    </Layout>
+  );
 };
 export default Write;
